b2b4c9ccdfbe4534f7fd768dc583f65aae9f46f0
nathalia <oliveiranathalia.mor@gmail.com>
2025-11-07 19:22:15 -0300

    Implementado classe Conta e add testes no Menu

--- Menu.ts
+++ Menu.ts
@@ -1,9 +1,17 @@
 import readlinesync = require("readline-sync");
 import { colors } from './src/util/Colors';
+import { Conta } from  './src/Model/Conta';
 
 export function main () {
 
     let opcao: number;
+
+    const conta: Conta = new Conta(1, 123, 1, "Nathalia", 1000);
+    conta.visualizar();
+    conta.sacar(10500);
+    conta.visualizar();
+    conta.depositar(5000);
+    conta.visualizar();
     
     while (true) {     
 
